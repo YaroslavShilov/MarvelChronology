44960da990b2baab7108224a881cf7951abf8bbe
Yaroslav Shilov <workyarik@gmail.com>
2020-05-25 18:29:46 +0300

    Updates

---
+++ precache-manifest.b7cbd572acd72dcf3acf6213d696032f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4301a60931db2fe9ae719b980312622",
+    "url": "/YaroslavShilov.github.io/Chronology/index.html"
+  },
+  {
+    "revision": "5f5e90ab65edb6e2fa68",
+    "url": "/YaroslavShilov.github.io/Chronology/static/css/main.8068b07f.chunk.css"
+  },
+  {
+    "revision": "9d1290967b81f93b0faa",
+    "url": "/YaroslavShilov.github.io/Chronology/static/js/2.f9c62f52.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/YaroslavShilov.github.io/Chronology/static/js/2.f9c62f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f5e90ab65edb6e2fa68",
+    "url": "/YaroslavShilov.github.io/Chronology/static/js/main.6fe9f1d3.chunk.js"
+  },
+  {
+    "revision": "59fcddcfb2f70aace14b",
+    "url": "/YaroslavShilov.github.io/Chronology/static/js/runtime-main.8ab5d79a.js"
+  },
+  {
+    "revision": "cbca34c9df1ebf66f702bae511afa06c",
+    "url": "/YaroslavShilov.github.io/Chronology/static/media/ComingSoon-Regular.cbca34c9.ttf"
+  },
+  {
+    "revision": "3bd167661809fbc135d78a75127050a5",
+    "url": "/YaroslavShilov.github.io/Chronology/static/media/bg.3bd16766.jpg"
+  },
+  {
+    "revision": "5b39fa8868071c2d2881ae5f89bf792f",
+    "url": "/YaroslavShilov.github.io/Chronology/static/media/logo.5b39fa88.png"
+  }
+]);
